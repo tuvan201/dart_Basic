4833c816a821aa6f62538b0883f2bb19f52cfb51
vantu <tnn209201@gmail.com>
2023-08-11 20:40:34 +0700

    c4

---
+++ future_ex2.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+class Product {
+  late double h, l, d, v, w, c;
+
+  Product(this.h, this.l, this.d);
+}
+
+calculate(List<Product> arr){
+  for(int i =0;i<arr.length;i++){
+    arr[i].v = arr[i].h * arr[i].l * arr[i].d;
+  }
+  print("đã tính xong thể tích của mỗi sản phẩm");
+}
+
+sumV(List<Product> arr) async{
+ Future.delayed(Duration(milliseconds: 10000),()
+  {
+    var sum= 0.0;
+    for(int i =0;i<arr.length;i++){
+      sum+= arr[i].v;
+    }
+    print(sum);
+  });
+}
+Future<void> main() async {
+  List<Product> arr = [];
+
+  stdout.write("Nhập số sản phẩm cần bán (N): ");
+  int n = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < n; i++) {
+    stdout.write("Nhập chiều cao của sản phẩm $i: ");
+    double h = double.parse(stdin.readLineSync()!);
+
+    stdout.write("Nhập chiều dài của sản phẩm $i: ");
+    double l = double.parse(stdin.readLineSync()!);
+
+    stdout.write("Nhập chiều rộng của sản phẩm $i: ");
+    double d = double.parse(stdin.readLineSync()!);
+
+    Product product = new Product(h, l, d);
+    arr.add(product);
+  }
+  await sumV(arr);
+  calculate(arr);
+
+  
+
+ 
+}
--- oop_ex2.dart
+++ oop_ex2.dart
@@ -1,60 +1,76 @@
 import 'dart:io';
 
-class Person{
-
+class Person {
   late String name;
-
   late int id;
 
   Person(this.name, this.id);
-
 }
 
-class Employee extends Person{
-
+class Employee extends Person {
   late List<double> salaries;
-
   late double average;
 
-  Employee(super.name, super.id, this.salaries, this.average);
-  
-  double? getAverage(){
-    double sum = 0.0;
-    for(var i = 0; i < salaries.length; i++){
-      sum += salaries[i];
+  Employee(String name, int id, this.salaries) : super(name, id);
+
+  void getAverage() {
+    average = salaries.reduce((value, element) => value + element) / salaries.length;
+  }
+  promote(){}
+}
+
+class Manager extends Employee {
+  Manager(String name, int id, List<double> salaries) : super(name, id, salaries);
+
+  void getAverage() {
+    average = salaries.reduce((value, element) => value + element) / salaries.length;
+  }
+
+  @override
+   promote() {
+    if (average >= 3000 && !salaries.any((salary) => salary < 3000)) {
+      return Manager(name, id, salaries);
+    } else {
+      return this;
     }
-    return average = sum / salaries.length;
   }
 }
 
- void main(List<String> args) {
+void main(List<String> args) {
+  stdout.write('Enter the number of months: ');
+  var n = int.parse(stdin.readLineSync()!);
 
-  stdout.write("Nhập số tháng lương mà nhân viên đã được nhận: ");
-  int n = int.parse(stdin.readLineSync()!); 
+  stdout.write('Enter the name: ');
+  var name = stdin.readLineSync()!;
 
-  stdout.write("Nhập tên: ");
-  String name = stdin.readLineSync()!;
-
-  stdout.write("nhập mã định danh: ");
-  int id = int.parse(stdin.readLineSync()!);
-
-  List<double> salaries = [];
-  for(int i = 0; i < n; i++){
-    stdout.write("nhập lương tháng $i: ");
-    double salary = double.parse(stdin.readLineSync()!);
-    salaries.add(salary);
+  stdout.write('Enter the ID: ');
+  var id = int.parse(stdin.readLineSync()!);
+  var employee;
+  List<double> arr=[];
+  for(int i =0;i<n;i++){
+    stdout.write('Enter the salary: ');
+    var salary = double.parse(stdin.readLineSync()!);
+    arr.add(salary);
   }
+  employee = Employee(name, id, arr);
+  employee.getAverage();
 
-  Employee em = Employee(name, id, salaries,0);
-  em.average = em.getAverage()!;
-
-  if (em.average < 1000) {
-    print("xếp hạng: D");
-  } else if (em.average >= 1000 && em.average < 2000) {
-    print("xếp hạng: C");
-  } else if (em.average >= 2000 && em.average < 3000) {
-    print("xếp hạng: B");
+  if (employee.average < 1000) {
+    print('Rank: D');
+  } else if (employee.average >= 1000 && employee.average < 2000) {
+    print('Rank: C');
+  } else if (employee.average >= 2000 && employee.average < 3000) {
+    print('Rank: B');
   } else {
-    print("xếp hạng: A");
+    print('Rank: A');
+  }
+  Manager mn  = Manager(name, id, arr);
+  mn.getAverage();
+  var mn1 = mn.promote();
+  if(mn1 is Manager){
+    print("len chuc thanh cong");
+  }
+  else{
+    print("chua du dieu kien");
   }
 }
